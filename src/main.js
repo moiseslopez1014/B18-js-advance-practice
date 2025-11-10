187d79987dd051de4490d2c2aff42c5fd8474bc5
moiseslopez <moiseslopeziglesias@gmail.com>
2025-11-10 09:51:32 +0100

    navBar

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import './scss/style.scss'
 
+const anchorElement = document.querySelector('#app');
+
+const settingsMovieBar = document.createElement('div');
+settingsMovieBar.className = 'settingsMovieBar';
+
+anchorElement.appendChild(settingsMovieBar);
+
+
+let viewingmode = 'grid';
+let viewingstatus = 'movies';
+

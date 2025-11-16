cceae693b891ae0ff0ae31b06aa02c9a588028c2
moiseslopez <moiseslopeziglesias@gmail.com>
2025-11-16 07:45:26 +0100

    mainMovieContainer

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 
+import { getMovies } from './scripts/api/fetching.js';
 import { showToolBar } from './scripts/utils/toolbar.js';
 import './scss/style.scss'
 
-const anchorElement = document.querySelector('#app');
 
+export let viewingMode = 'movie-grid'; //ALTERNATE WITH LIST OR GRID
+export let viewingStatus = 'movies';// ALTERNATE TO SHOW MOVIES OR CREDITS
+
+
+
+const anchorElement = document.querySelector('#app');
+//TOOLBAR WHIT CATEGORY SELECTOR
 export const toolbarMovies = document.createElement('div');
 toolbarMovies.className = 'settingsMovieBar';
 
 anchorElement.appendChild(toolbarMovies);
 
+// MAIN CONTAINER FOR MOVIES
+const mainFrame = document.createElement('section');
+mainFrame.className = viewingMode;
 
-export let viewingMode = 'grid';
-export let viewingStatus = 'movies';
+anchorElement.appendChild(mainFrame);
 
+
+//VARIABLES TO DETERMINE MODE OF VIEW AND SECTION
+
+
+// FUNCTION TO CREATE AND SHOW TOOLBAR ELEMENTS
 showToolBar(toolbarMovies, viewingStatus);
+//FUNCTION TO GET CONTENT
+getMovies(mainFrame, 'now_playing', '');
 

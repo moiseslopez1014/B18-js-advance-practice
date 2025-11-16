26256c75432a7a900896ea04e37871d5ff3de7cb
moiseslopez <moiseslopeziglesias@gmail.com>
2025-11-16 16:00:57 +0100

    changeview5

--- src/scripts/modules/state.js
+++ src/scripts/modules/state.js
@@ -1,11 +1,10 @@
-// state.js
-export let viewingMode = 'movie-grid';
-export let viewingStatus = 'movies';
+export let viewingMode = "movie-grid";
+export let viewingStatus = "movies";
 
 export function setViewingMode(mode) {
-  viewingMode = mode;
+    viewingMode = mode;
 }
 
 export function setViewingStatus(status) {
-  viewingStatus = status;
+    viewingStatus = status;
 }
